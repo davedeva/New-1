12f868d82988c899637b8f4b47e42cecc867f6a7
Dawit Desalegn <davedeva841@gmail.com>
2025-11-19 11:02:15 +0300

    Add files via upload

---
+++ pro1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x, y, sum;
+cout<<"enter the first no";
+cin>>x;
+cout<<"enter the second no";
+cin>>y;
+sum=x+y;
+cout<<"sum="<<sum<<endl;
+
+
+
+return 0;
+
+
+}
